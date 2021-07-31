a84a8923d60710d821e97c490a5a869918eab667
Eduardo Tavares <edutav@gmail.com>
2021-07-31 15:25:12 -0400

    implementação da função CriarUsuario no controller

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,10 +1,36 @@
 package controllers
 
-import "net/http"
+import (
+	"devbook/src/database"
+	"devbook/src/models"
+	"devbook/src/repositories"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 // Cria usuário no banco de dados
 func CriarUsuario(rw http.ResponseWriter, r *http.Request) {
 	rw.Write([]byte("Criando usuário"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	user := models.Usuario{}
+
+	if erro = json.Unmarshal(corpoRequest, &user); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := database.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositoryUsuario := repositories.NovoRepositorioUsuario(db)
+	repositoryUsuario.Criar(user)
 }
 
 // Lista usuários no banco de dados
