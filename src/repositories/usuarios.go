3a5d17e223afc22d8f3e5e0b581152a31fed4e68
Eduardo Tavares <edutav@gmail.com>
2021-08-01 10:04:48 -0400

    implementação da função criar

--- src/repositories/usuarios.go
+++ src/repositories/usuarios.go
@@ -16,5 +16,21 @@ func NovoRepositorioUsuario(db *sql.DB) *UsuarioRepository {
 
 // Insise um usuário no banco
 func (ur UsuarioRepository) Criar(usuario models.Usuario) (uint64, error) {
-	return 0, nil
+	statement, erro := ur.db.Prepare("insert into usuarios(nome, nick, email, senha) values(?, ?, ?, ?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	result, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIdInserido, erro := result.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIdInserido), nil
 }
