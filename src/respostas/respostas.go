b4bc2a8aaed67e022d4f5a5618516e8daa9073f2
Eduardo Tavares <edutav@gmail.com>
2021-08-01 12:08:41 -0400

    implementação de respostas para sucesso e erro

---
+++ src/respostas/respostas.go
@@ -0,0 +1,23 @@
+package respostas
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Retorna uma resposta em Json
+func JSON(rw http.ResponseWriter, statusCode int, dados interface{}) {
+	rw.WriteHeader(statusCode)
+	if erro := json.NewEncoder(rw).Encode(dados); erro != nil {
+		log.Fatal(erro)
+	}
+
+}
+
+// Retorna uma mensagem em formato json
+func Erro(rw http.ResponseWriter, statusCode int, erro error) {
+	JSON(rw, statusCode, struct {
+		Erro string `json:"erro"`
+	}{Erro: erro.Error()})
+}
