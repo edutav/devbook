e8626725ab3e3d2c2e4443260a7dad1b7a32793c
Eduardo Tavares <edutav@gmail.com>
2021-08-01 12:09:00 -0400

    alterado o atributo id para uint64

--- src/models/usuario.go
+++ src/models/usuario.go
@@ -4,7 +4,7 @@ import "time"
 
 // Representa um usuário utilizando a rede social
 type Usuario struct {
-	ID       uint      `json:"id,omitempty"`
+	ID       uint64    `json:"id,omitempty"`
 	Nome     string    `json:"nome,omitempty"`
 	Nick     string    `json:"nick,omitempty"`
 	Email    string    `json:"email,omitempty"`
