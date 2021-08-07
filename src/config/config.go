99ddd2040f1300bf1ef4ebeffa6a99bd10824219
Eduardo Tavares <edutav@gmail.com>
2021-08-07 14:42:00 -0400

    adicionando leitura da secret kay

--- src/config/config.go
+++ src/config/config.go
@@ -12,6 +12,7 @@ import (
 var (
 	StringConexaoBanco = ""
 	Porta              = 0
+	SecretKey          []byte
 )
 
 // Inicializar as variaveis de ambiente
@@ -30,4 +31,6 @@ func Carregar() {
 	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("DB_USUARIO"), os.Getenv("DB_SENHA"), os.Getenv("DB_NOME"),
 	)
+
+	SecretKey = []byte(os.Getenv("SECRET_KEY"))
 }
