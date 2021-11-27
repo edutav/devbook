7bb64819850a56b99d55219f367769e49fcd28aa
Eduardo Tavares <edutav@gmail.com>
2021-11-27 15:52:54 -0400

    adicionada função que busca na base os seguidores

--- src/repositories/usuarios.go
+++ src/repositories/usuarios.go
@@ -156,3 +156,26 @@ func (ur usuarioRepository) PararDeSeguir(usuarioID, seguidorID uint64) error {
 
 	return nil
 }
+
+// Busca todos os seguidores de um usuário
+func (ur usuarioRepository) BuscarSeguidores(usuarioID uint64) ([]models.Usuario, error) {
+	linhas, erro := ur.db.Query(`select u.id, u.nome, u.nick, u.email, u.criadoEm from usuarios u inner join seguidores s on u.id = s.seguidor_id where s.usuario_id = ?`, usuarioID)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var users []models.Usuario
+
+	for linhas.Next() {
+		var user = models.Usuario{}
+
+		if erro = linhas.Scan(&user.ID, &user.Nome, &user.Nick, &user.Email, &user.CriadoEm); erro != nil {
+			return nil, erro
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
